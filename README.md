71bbdda9e83aee2e10cfbdad8c4190f1e840ac4d
eyarga <yargaelvis@gmail.com>
2023-05-21 19:34:04 -0400

    Commit all files for translation in different language

---
+++ README.md
@@ -0,0 +1,4 @@
+Template of multi language website.
+This allow to have a unique file for all translation in different language on the website.
+
+Do not forget to add the script line in the different pages like in the index.html
---
+++ translations.js
@@ -0,0 +1,18 @@
+const translations = {
+    en: {
+      home: "Home",
+      about: "About",
+      contactUs: "Contact us",
+      english: "English",
+      french: "French",
+    },
+    fr: {
+      home: "Accueil",
+      about: "A propos",
+      contactUs: "Contactez-nous",
+      english: "Anglais",
+      french: "Français",
+    },
+  };
+  
+  export default translations;
